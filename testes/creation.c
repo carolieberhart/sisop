5e3f0d4469aa399c26548f8d321e45833e6da9e5
João Weit <jpweit@gmail.com>
2015-05-12 23:29:47 -0300

    movendo arquivos

--- testes/creation.c
+++ testes/creation.c
@@ -2,13 +2,7 @@
 #include <stdlib.h>
 #include "../include/mthread.h"
 
-/*
 
-Este teste demonstra como é possível criar um número "infinito" de threads, até que a memória fique cheia.
-Ao chamar o programa é passado um argumento arg do tipo inteiro que cria um número 'arg' de threads. Se nada
-for informado, o número default de threads criadas é 20.
-
-*/
 void doSomething(void *arg)
 {
 
@@ -20,27 +14,25 @@ void doSomething(void *arg)
 
 int main(int arg, char **argcv){
 
-int nT=20;
-	if(arg > 0)
-		nT=arg;
+	int nT=10;
+		
 
-int *threads = (int*)malloc(nT*sizeof(int));
-int i;
-int n=0;
+	int *threads = (int*)malloc(nT*sizeof(int));
+	int i;
+	int n=0;
 
 
-if(threads==NULL)
-    return ;
-
-for(i=0;i<nT;i++){
-    threads[i]=mcreate(1, doSomething, &n);
-}
-
+	if(threads==NULL)
+		return 0;
+	for(i=0;i<nT;i++){
+		threads[i]=mcreate(i%2, (void*)doSomething, &n);
+	}
+	mwait(3);
 
 
 
 
-printf("\nEnd of main...\n");
 
-
+	printf("\nEnd of main...\n");
+	return 0;
 }
